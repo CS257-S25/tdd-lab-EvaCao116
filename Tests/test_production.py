f4187c6acbb3f78c555ec8a37c40015232333fcf
Eva Cao <122134995+EvaCao116@users.noreply.github.com>
2025-04-11 13:52:18 -0500

    0411

--- Tests/test_production.py
+++ Tests/test_production.py
@@ -1,3 +1,17 @@
 '''
 A file for tests for the production code.
 '''
+import unittest
+from production import reverse_word
+
+class TestReverseWord(unittest.TestCase):
+    def test_reverse_word(self):
+        """
+        Test that it can reverse the word
+        """
+        word = "Seven"
+        result = reverse_word(word)
+        self.assertEqual(result, "neveS")
+
+if __name__ == '__main__':
+    unittest.main()
